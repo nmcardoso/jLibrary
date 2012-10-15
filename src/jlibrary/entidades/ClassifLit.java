7211ef9c68c29162c53ddc794a8c4bf8c6c8a7a2
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:40 -0300

    Classificação Literária

---
+++ src/jlibrary/entidades/ClassifLit.java
@@ -0,0 +1,28 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class ClassifLit {
+    private int idClassLit;
+    private String classificacao;
+
+    public int getIdClassLit() {
+        return idClassLit;
+    }
+
+    public void setIdClassLit(int idClassLit) {
+        this.idClassLit = idClassLit;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+    
+    
+}
