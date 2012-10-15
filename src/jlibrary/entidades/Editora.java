b95eba38c7e91d9220d2dff4f7ea87c8e50d2fbd
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:44 -0300

    Editora

---
+++ src/jlibrary/entidades/Editora.java
@@ -0,0 +1,29 @@
+
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Editora {
+    private int idEditora;
+    private String editora;
+
+    public int getIdEditora() {
+        return idEditora;
+    }
+
+    public void setIdEditora(int idEditora) {
+        this.idEditora = idEditora;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    
+    
+}
