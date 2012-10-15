5cc3cf0ce9786e019a67c456dc5704728caaa3c8
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:48 -0300

    TipoItem

---
+++ src/jlibrary/entidades/TipoItem.java
@@ -0,0 +1,29 @@
+
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class TipoItem {
+    private int idTipo;
+    private String tipo;
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
