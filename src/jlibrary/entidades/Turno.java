1274049bbd8310cb1a952a2de14bbd325c1e544a
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:47 -0300

    Turno

---
+++ src/jlibrary/entidades/Turno.java
@@ -0,0 +1,27 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Turno {
+
+    private int idTurno;
+    private String nome;
+
+    public int getIdTurno() {
+        return idTurno;
+    }
+
+    public void setIdTurno(int idTurno) {
+        this.idTurno = idTurno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
