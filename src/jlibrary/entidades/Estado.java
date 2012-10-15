9bab7931c6f722c897188a8207434eae8c1725e4
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:45 -0300

    Estado

---
+++ src/jlibrary/entidades/Estado.java
@@ -0,0 +1,36 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Estado {
+
+    private int idEstado;
+    private String nome;
+    private String uf;
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
