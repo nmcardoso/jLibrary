32eca1d0a23507d52a77e3654b27d241a3efc40f
Natanael <natanael_magalhaes@msn.com>
2012-10-15 17:13:40 -0300

    Cidade

---
+++ src/jlibrary/entidades/Cidade.java
@@ -0,0 +1,37 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Cidade {
+
+    private int idCidade;
+    private int idEstado;
+    private String nome;
+
+    public int getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(int idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
