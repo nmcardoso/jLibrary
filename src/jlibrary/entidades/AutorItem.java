2505b2f0bf5e089013c9cb5bc041cf335ccf415a
Natanael <natanael_magalhaes@msn.com>
2012-10-19 16:45:12 -0300

    relação autor-item

---
+++ src/jlibrary/entidades/AutorItem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class AutorItem {
+    private int idAutorItem;
+    private int idAutor;
+    private int idItem;
+
+    public int getIdAutorItem() {
+        return idAutorItem;
+    }
+
+    public void setIdAutorItem(int idAutorItem) {
+        this.idAutorItem = idAutorItem;
+    }
+
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+}
