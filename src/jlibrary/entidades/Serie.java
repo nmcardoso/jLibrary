dbe45e3cdcaf5246e5dcf9679bc1564768dc1bd3
Natanael <natanael_magalhaes@msn.com>
2012-11-08 20:14:24 -0200

    Série

---
+++ src/jlibrary/entidades/Serie.java
@@ -0,0 +1,47 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Serie {
+
+    private int idSerie;
+    private String nome;
+    private int idTurno;
+    private Turno turno;
+    
+    public int getIdSerie() {
+        return idSerie;
+    }
+
+    public void setIdSerie(int idSerie) {
+        this.idSerie = idSerie;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdTurno() {
+        return idTurno;
+    }
+
+    public void setIdTurno(int idTurno) {
+        this.idTurno = idTurno;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+    
+
+}
