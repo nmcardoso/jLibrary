e69b6105b507873e51ffc5ea0b399906e815544e
Natanael <natanael_magalhaes@msn.com>
2012-11-11 15:03:57 -0200

    modelo Leitor

---
+++ src/jlibrary/entidades/Leitor.java
@@ -0,0 +1,210 @@
+package jlibrary.entidades;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Leitor {
+
+    private int idLeitor;
+    private String dataNasc;
+    private String dataCad;
+    private String sexo;
+    private String nome;
+    private String nomePai;
+    private String nomeMae;
+    private String rg;
+    private String cpf;
+    private String cep;
+    private String endereco;
+    private String numEnd;
+    private String bairro;
+    private String email;
+    private String telefone1;
+    private String telefone2;
+    private String celular1;
+    private String celular2;
+    private Serie serie;
+    private Turno turno;
+    private Cidade cidade;
+    private Estado estado;
+
+    public int getIdLeitor() {
+        return idLeitor;
+    }
+
+    public void setIdLeitor(int idLeitor) {
+        this.idLeitor = idLeitor;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataCad() {
+        return dataCad;
+    }
+
+    public void setDataCad(String dataCad) {
+        this.dataCad = dataCad;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumEnd() {
+        return numEnd;
+    }
+
+    public void setNumEnd(String numEnd) {
+        this.numEnd = numEnd;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone1() {
+        return telefone1;
+    }
+
+    public void setTelefone1(String telefone1) {
+        this.telefone1 = telefone1;
+    }
+
+    public String getTelefone2() {
+        return telefone2;
+    }
+
+    public void setTelefone2(String telefone2) {
+        this.telefone2 = telefone2;
+    }
+
+    public String getCelular1() {
+        return celular1;
+    }
+
+    public void setCelular1(String celular1) {
+        this.celular1 = celular1;
+    }
+
+    public String getCelular2() {
+        return celular2;
+    }
+
+    public void setCelular2(String celular2) {
+        this.celular2 = celular2;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+    
+}
